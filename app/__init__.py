d7417e2c7adc52990f0cb3023a030a46e78767fc
oluwafemi medalem <oluwafemi.medale@andela.com>
2017-08-25 11:15:50 +0100

    setting up migration and other configurations

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,34 @@
+# third-party imports
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_migrate import Migrate
 
-# Initialize the app
-app = Flask(__name__, instance_relative_config=True)
+# local imports
+from config import app_config
 
-# Load the views
-from app import views
+# db variable initialization
+db = SQLAlchemy()
+login_manager = LoginManager()
 
-# Load the config file
-app.config.from_object('config')
+def create_app(config_name):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_object(app_config[config_name])
+    app.config.from_pyfile('config.py')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+    db.init_app(app)
+
+      # temporary route
+    @app.route('/')
+    def hello_world():
+        return 'Hello, World!'
+
+    login_manager.init_app(app)
+    login_manager.login_message = "You must be logged in to access this page."
+    login_manager.login_view = "auth.login"
+
+    migrate = Migrate(app, db)
+
+    from app import models
+
+    return app
