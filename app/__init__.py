53747fba3099dcaa0735d446b4ea3ed6cb034369
oluwafemi medalem <oluwafemi.medale@andela.com>
2017-08-25 14:37:06 +0100

    regsitration and login

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,9 @@
 # third-party imports
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
 # local imports
 from config import app_config
@@ -16,12 +17,9 @@ def create_app(config_name):
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-    db.init_app(app)
 
-      # temporary route
-    @app.route('/')
-    def hello_world():
-        return 'Hello, World!'
+    Bootstrap(app)
+    db.init_app(app)
 
     login_manager.init_app(app)
     login_manager.login_message = "You must be logged in to access this page."
@@ -30,5 +28,15 @@ def create_app(config_name):
     migrate = Migrate(app, db)
 
     from app import models
+
+    from .admin import admin as admin_blueprint
+    app.register_blueprint(admin_blueprint, url_prefix='/admin')
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+
+    from .home import home as home_blueprint
+    app.register_blueprint(home_blueprint)
+
 
     return app
