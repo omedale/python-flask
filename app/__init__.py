cd2cc07830d7ee0e3a69451f73704c761fffd4d5
oluwafemi medalem <oluwafemi.medale@andela.com>
2017-08-28 18:35:12 +0100

    edit req.txt

--- app/__init__.py
+++ app/__init__.py
@@ -15,16 +15,9 @@ db = SQLAlchemy()
 login_manager = LoginManager()
 
 def create_app(config_name):
-    if os.getenv('FLASK_CONFIG') == "production":
-        app = Flask(__name__)
-        app.config.update(
-            SECRET_KEY=os.getenv('SECRET_KEY'),
-            SQLALCHEMY_DATABASE_URI=os.getenv('SQLALCHEMY_DATABASE_URI')
-        )
-    else:
-        app = Flask(__name__, instance_relative_config=True)
-        app.config.from_object(app_config[config_name])
-        app.config.from_pyfile('config.py')
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_object(app_config[config_name])
+    app.config.from_pyfile('config.py')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
     Bootstrap(app)
